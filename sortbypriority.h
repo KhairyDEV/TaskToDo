b55edef8b8ccc583869623fb9da78db704757d95
Amr Alashwat <A.khaled2463@nu.edu.eg>
2025-05-24 00:34:03 +0300

    Add files via upload

---
+++ sortbypriority.h
@@ -0,0 +1,10 @@
+#ifndef SORTBYPRIORITY_H
+#define SORTBYPRIORITY_H
+
+#include <unordered_map>
+#include <string>
+#include "taskobject.h"
+
+void sortbypriority(unordered_map<string, TaskObject>& tasksList);
+
+#endif
