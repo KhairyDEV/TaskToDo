96dcfbe0cf0a0a1917084eb2aa7ef8e67eb65b60
youssefshash <y.waleed2487@nu.edu.eg>
2025-05-24 23:00:35 +0300

    updated to ignore spaces at the end

--- deletetask.cpp
+++ deletetask.cpp
@@ -13,6 +13,7 @@ void DeleteTask(unordered_map<string, TaskObject>& tasksList)
         cout << "No tasks to delete.\n";
         return;
     }
+
     cout << "Current Tasks:\n";
     for (const auto& pair : tasksList) {
         cout << "- " << pair.first << endl;
@@ -21,22 +22,17 @@ void DeleteTask(unordered_map<string, TaskObject>& tasksList)
     cout << "Enter the title of the task to delete: ";
     string titlefordeleting;
     getline(cin >> ws, titlefordeleting);
-
-    string titlecorrected = titlefordeleting;
-    transform(titlecorrected.begin(), titlecorrected.end(), titlecorrected.begin(), ::tolower);
     
-    for (auto it = tasksList.begin(); it != tasksList.end(); ++it) {
-        string currentcorrectedtask = it->first;
-        transform(currentcorrectedtask.begin(), currentcorrectedtask.end(), currentcorrectedtask.begin(), ::tolower);
+    titlefordeleting = trim(titlefordeleting); 
+    transform(titlefordeleting.begin(), titlefordeleting.end(), titlefordeleting.begin(), ::tolower);
 
-        if (currentcorrectedtask == titlecorrected) {
-            string deletedTitle = it->first;  
-            tasksList.erase(it);              
-            cout << "Task \"" << deletedTitle << "\" deleted successfully.\n";
-            return;
-        }
+    auto it = tasksList.find(titlefordeleting);
+    if (it != tasksList.end()) {
+        string deletedTitle = it->first;
+        tasksList.erase(it);
+        cout << "Task \"" << deletedTitle << "\" deleted successfully.\n";
+    } else {
+        cout << "Task \"" << titlefordeleting << "\" not found.\n";
     }
-
-    cout << "Task \"" << titlefordeleting << "\" not found.\n";
 }
 
